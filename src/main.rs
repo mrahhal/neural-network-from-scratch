8782bf23644cebfa53e6f153ccb56ae81f6e6804
Mohammad Rahhal <m.r992@hotmail.com>
2022-12-23 20:42:23 +0100

    Update target acc

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ fn main() {
 
     println!("===");
 
-    let target_accuracy = 0.95;
+    let target_accuracy = 0.9;
 
     println!(
         "Training and validating accuracy after each epoch. Target accuracy is {}%.",
